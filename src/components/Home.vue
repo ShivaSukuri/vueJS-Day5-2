9a34cbf13108da6f18aaef7f92f799e6a109ef7e
ShivaSukuri <shivasukuri2000@gmail.com>
2024-02-14 18:24:06 +0530

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="content">
+<a :href="MTrailer" style="  text-decoration:none;
+    color:black;">
+<img :src="MSrc">
+<h2>{{MName}}</h2></a>
+</div>
+</template>
+<script setup>
+defineProps(["MName","MSrc","MTrailer"])
+</script>
+<style>
+.content {
+  border: 2px solid black;
+  height: 350px;
+  width: 100%; 
+}
+
+.content img {
+  width: 100%; 
+  height: 250px;
+
+}
+
+.content h2 {
+  margin: 0;
+  padding: 8px;
+  text-align: center;
+}
+/* a{
+    text-decoration:none;
+    color:black;
+} */
+</style>
